6b2d0b9a5494c75a46438e4c926d96f423a6d598
diskree <diskreee@gmail.com>
2024-06-22 03:05:28 +0400

    1.15.1+

--- src/main/java/com/diskree/mc229142fix/mixin/ItemRendererMixin.java
+++ src/main/java/com/diskree/mc229142fix/mixin/ItemRendererMixin.java
@@ -3,24 +3,29 @@ package com.diskree.mc229142fix.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.client.render.item.ItemModels;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.BakedModelManager;
 import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.asm.mixin.Final;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ItemRenderer.class)
-public class ItemRendererMixin {
+public abstract class ItemRendererMixin {
 
     @Shadow
-    @Final
-    private ItemModels models;
+    protected abstract BakedModel getOverriddenModel(
+        BakedModel model,
+        ItemStack stack,
+        @Nullable World world,
+        @Nullable LivingEntity entity
+    );
 
     @WrapOperation(
         method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
@@ -36,16 +41,11 @@ public class ItemRendererMixin {
         @Local(argsOnly = true) ItemStack stack,
         @Local(argsOnly = true) BakedModel model
     ) {
-        BakedModel spyglassOrTridentModel = original.call(modelManager, id);
-        BakedModel customSpyglassOrTridentModel = spyglassOrTridentModel.getOverrides().apply(
-            spyglassOrTridentModel,
+        return getOverriddenModel(
+            original.call(modelManager, id),
             stack,
             null,
             null
         );
-        if (customSpyglassOrTridentModel == null) {
-            return models.getModelManager().getMissingModel();
-        }
-        return customSpyglassOrTridentModel;
     }
 }
