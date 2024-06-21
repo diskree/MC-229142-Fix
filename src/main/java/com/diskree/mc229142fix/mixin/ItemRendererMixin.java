33b6ba29b8557db8e0db96f90a055b07e19cfcd7
diskree <diskreee@gmail.com>
2024-06-22 02:46:53 +0400

    1.17+

--- src/main/java/com/diskree/mc229142fix/mixin/ItemRendererMixin.java
+++ src/main/java/com/diskree/mc229142fix/mixin/ItemRendererMixin.java
@@ -23,7 +23,7 @@ public class ItemRendererMixin {
     private ItemModels models;
 
     @WrapOperation(
-        method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
+        method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/client/render/model/BakedModelManager;getModel(Lnet/minecraft/client/util/ModelIdentifier;)Lnet/minecraft/client/render/model/BakedModel;"
