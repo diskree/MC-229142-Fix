7eb97a74e16d27cc7d580df201a19bfe59b6c65f
diskree <diskreee@gmail.com>
2024-06-22 02:16:05 +0400

    Fix nullable custom model

--- src/main/java/com/diskree/mc229142fix/mixin/ItemRendererMixin.java
+++ src/main/java/com/diskree/mc229142fix/mixin/ItemRendererMixin.java
@@ -3,17 +3,24 @@ package com.diskree.mc229142fix.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.render.item.ItemModels;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.BakedModelManager;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ItemRenderer.class)
 public class ItemRendererMixin {
+
+    @Shadow
+    @Final
+    private ItemModels models;
 
     @WrapOperation(
         method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
@@ -30,6 +37,16 @@ public class ItemRendererMixin {
         @Local(argsOnly = true) BakedModel model
     ) {
         BakedModel spyglassOrTridentModel = original.call(modelManager, id);
-        return spyglassOrTridentModel.getOverrides().apply(spyglassOrTridentModel, stack, null, null, 0);
+        BakedModel customSpyglassOrTridentModel = spyglassOrTridentModel.getOverrides().apply(
+            spyglassOrTridentModel,
+            stack,
+            null,
+            null,
+            0
+        );
+        if (customSpyglassOrTridentModel == null) {
+            return models.getModelManager().getMissingModel();
+        }
+        return customSpyglassOrTridentModel;
     }
 }
