2dc1bc7bccbf2c1f0403301769e0fa1fb46e001a
diskree <diskreee@gmail.com>
2024-10-28 02:14:30 +0400

    1.21.2+

--- src/main/java/com/diskree/mc229142fix/mixin/ItemRendererMixin.java
+++ src/main/java/com/diskree/mc229142fix/mixin/ItemRendererMixin.java
@@ -1,52 +1,37 @@
 package com.diskree.mc229142fix.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.client.render.item.ItemModels;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.BakedModelManager;
 import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
-import org.jetbrains.annotations.NotNull;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ItemRenderer.class)
-public class ItemRendererMixin {
+public abstract class ItemRendererMixin {
 
     @Shadow
-    @Final
-    private ItemModels models;
+    public abstract BakedModel getModel(ItemStack stack, @Nullable World world, @Nullable LivingEntity entity, int seed);
 
-    @WrapOperation(
-        method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
+    @Redirect(
+        method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;Z)V",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/client/render/model/BakedModelManager;getModel(Lnet/minecraft/client/util/ModelIdentifier;)Lnet/minecraft/client/render/model/BakedModel;"
         )
     )
     private BakedModel applyModelOverridesForSpyglassAndTrident(
-        BakedModelManager modelManager,
+        BakedModelManager instance,
         ModelIdentifier id,
-        @NotNull Operation<BakedModel> original,
-        @Local(argsOnly = true) ItemStack stack,
-        @Local(argsOnly = true) BakedModel model
+        @Local(argsOnly = true) ItemStack stack
     ) {
-        BakedModel spyglassOrTridentModel = original.call(modelManager, id);
-        BakedModel customSpyglassOrTridentModel = spyglassOrTridentModel.getOverrides().apply(
-            spyglassOrTridentModel,
-            stack,
-            null,
-            null,
-            0
-        );
-        if (customSpyglassOrTridentModel == null) {
-            return models.getModelManager().getMissingModel();
-        }
-        return customSpyglassOrTridentModel;
+        return getModel(stack, null, null, 0);
     }
 }
